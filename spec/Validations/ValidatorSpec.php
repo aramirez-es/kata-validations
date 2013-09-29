5b81e9aaa206e68bf190d2259955bcfcf4b9857b
Alberto Ramirez <alberto@aramirez.es>
2013-09-29 22:16:39 +0200

    Fix indentation

--- spec/Validations/ValidatorSpec.php
+++ spec/Validations/ValidatorSpec.php
@@ -7,23 +7,23 @@ use Prophecy\Argument;
 
 class ValidatorSpec extends ObjectBehavior
 {
-	public function it_should_return_true_if_number_is_valid()
-	{
-		$this->isValid(4, ['I'])->shouldReturn(true);
-	}
+    public function it_should_return_true_if_number_is_valid()
+    {
+        $this->isValid(4, ['I'])->shouldReturn(true);
+    }
 
-	public function it_should_return_false_if_number_is_not_valid()
-	{
-		$this->isValid('4', ['I'])->shouldReturn(false);
-	}
+    public function it_should_return_false_if_number_is_not_valid()
+    {
+        $this->isValid('4', ['I'])->shouldReturn(false);
+    }
 
-	public function it_should_return_true_if_string_is_valid()
-	{
-		$this->isValid('4', ['S'])->shouldReturn(true);
-	}
+    public function it_should_return_true_if_string_is_valid()
+    {
+        $this->isValid('4', ['S'])->shouldReturn(true);
+    }
 
-	public function it_should_return_false_if_string_is_not_valid()
-	{
-		$this->isValid(4, ['S'])->shouldReturn(false);
-	}
+    public function it_should_return_false_if_string_is_not_valid()
+    {
+        $this->isValid(4, ['S'])->shouldReturn(false);
+    }
 }
--- src/Validations/Validator.php
+++ src/Validations/Validator.php
@@ -4,12 +4,12 @@ namespace Validations;
 
 class Validator
 {
-	public function isValid($input, $type)
-	{
-    	if ( $type[0] == 'S' ){
-    		return is_string( $input );
-    	}
+    public function isValid($input, $type)
+    {
+        if ( $type[0] == 'S' ){
+            return is_string( $input );
+        }
 
         return is_int( $input );
-	}
+    }
 }
