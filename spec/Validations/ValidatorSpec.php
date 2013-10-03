46f32620f83255814c4b80a791117b8005135565
Alberto Ramirez <alberto@aramirez.es>
2013-10-03 20:09:45 +0200

    complete validations

--- spec/Validations/ValidatorSpec.php
+++ spec/Validations/ValidatorSpec.php
@@ -26,4 +26,36 @@ class ValidatorSpec extends ObjectBehavior
     {
         $this->isValid(4, ['S'])->shouldReturn(false);
     }
+
+    public function it_should_return_true_if_length_of_number_is_valid()
+    {
+        $this->isValid(40, ['I', 2])->shouldReturn(true);
+    }
+
+    public function it_should_return_false_if_length_of_number_is_not_valid()
+    {
+        $this->isValid(40, ['I', 3])->shouldReturn(false);
+    }
+
+    public function it_should_return_true_if_length_of_string_is_valid()
+    {
+        $this->isValid('40', ['S', 2])->shouldReturn(true);
+    }
+
+    public function it_should_return_false_if_length_of_string_is_not_valid()
+    {
+        $this->isValid('40', ['S', 3])->shouldReturn(false);
+    }
+
+    public function it_should_return_true_if_there_are_no_validations_to_do()
+    {
+        $this->isValid('40', [])->shouldReturn(true);
+    }
+
+    public function it_should_throw_an_exception_if_validation_type_is_not_expected()
+    {
+        $this
+            ->shouldThrow(new \UnexpectedValueException('Validation type unknow'))
+            ->duringIsValid('40', ['U']);
+    }
 }
--- src/Validations/Validator.php
+++ src/Validations/Validator.php
@@ -4,12 +4,43 @@ namespace Validations;
 
 class Validator
 {
-    public function isValid($input, $type)
+    private $validation_map = [
+        'S' => 'is_string',
+        'I' => 'is_int',
+    ];
+
+    public function isValid($input, $validation_rules)
     {
-        if ( $type[0] == 'S' ){
-            return is_string( $input );
+        if (empty($validation_rules)) {
+            return true;
         }
 
-        return is_int( $input );
+        if ($this->isValidationTypeRight($validation_rules[0])){
+
+            $is_valid = $this->isValidType($input, $validation_rules[0]);
+
+            if ($is_valid && !empty($validation_rules[1])) {
+                $is_valid = $this->isValidLength($input, $validation_rules[1]);
+            }
+
+            return $is_valid;
+        }
+
+        throw new \UnexpectedValueException('Validation type unknow');
+    }
+
+    private function isValidationTypeRight($type)
+    {
+        return isset($this->validation_map[$type]);
+    }
+
+    private function isValidType($input, $type)
+    {        
+        return $this->validation_map[$type]( $input );
+    }
+
+    private function isValidLength($input, $length)
+    {
+        return strlen($input) === $length;
     }
 }
